ed52ec97d8d3b374ac2d340542196b174da97143
Martin Glatzle <mglatzle@mpa-garching.mpg.de>
2019-01-31 16:57:45 +0100

    Restructure.

--- cosmic_dustbox/gspecies.py
+++ cosmic_dustbox/gspecies.py
@@ -14,119 +14,17 @@ import Solver as sv
 
 
 class GrainSpecies(object):
-    
-    loadFiles = { 'carbon_par_0.01': 'callindex.out_CpaD03_0.01.txt', 
-                    'carbon_par_0.1':'callindex.out_CpaD03_0.10.txt',
-                    'carbon_per_0.01':'callindex.out_CpeD03_0.01.txt' ,
-                    'carbon_per_0.1': 'callindex.out_CpeD03_0.10.txt',
-                    'silica': 'callindex.out_silD03.txt' }
 
-
-    def __init__(self, en, sizes):
-        self.en = en
-        self.sizes = sizes
+    def __init__(self):
         return
 
 
-    def __add__(self, other):
-        if type(other) == self.__class__:
-            def s_abs(*args):
-                return self.s_abs(*args) + \
-                    other.s_abs(*args)
-
-            def s_sca(*args):
-                return self.s_sca(*args) + \
-                    other.s_sca(*args)
-            return self.__class__(s_abs, s_sca)
-        else:
-            def s_abs(*args):
-                return other + self.s_abs(*args)
-
-            def s_sca(*args):
-                return other + self.s_sca(*args)
-            return self.__class__(s_abs, s_sca)
-
-    __radd__ = __add__
-
-
-    def __mul__(self, other):
-        def s_abs(*args):
-            return other*self.s_abs(*args)
-
-        def s_sca(*args):
-            return other*self.s_sca(*args)
-
-        return self.__class__(s_abs, s_sca)
-
-    __rmul__ = __mul__
-
-
-        @staticmethod
-    def loadOprops(path):
-        """
-        Load the optical properties files in CSV format.
-        
-
-        'path': path to Refractive Index and Material properties file
-
-        returns:
-            energy (1D) [eV] at which Q values where computed
-            Real Refractive index and Imaginary Refractive index 
-        """
-            rel_path = '\\lib\\' + path
-            path = os.path.dirname(os.path.realpath(__file__)) + rel_path
-
-        data = []
-        with open(path) as f:
-            for line in f:
-                if 'number of wavelengths' not in line:
-                    continue
-                else:
-                    line = f.next().split()
-                    w = []
-                    eps_1 = []
-                    eps_2 = []
-                    real_n = []
-                    Im_n = []
-                    while line:
-                        try:
-                            line = f.next().split()
-                        except StopIteration:
-                            line = []
-                        if line:
-                            w.append(float(line[0]))
-                            Re_n.append(float(line['Re(n)-1']) + 1)
-                            Im_n.append(float(line['Im(n)']))
-                    data.append([
-                        _np.array(w),
-                        _np.array(Re_n),
-                        _np.array(Im_n)])
-
-             return data
-                                
-    def Find_Interpolate(lamda,array):
-        w = array[0]
-        if lamda in w:
-            ind = _np.where(w == lamda)
-            Re_n_out = array[1][ind]
-            Im_n_out = array[2][ind]
-        else:    
-            ind1 = _np.where(w >= lamda).min()
-            ind2 = _np.where(w <= lamda).max()
-            wght1 = (w[ind1] - lamda)/lamda
-            wght2= (lamda - w[ind2]) / lamda         
-            Re_n_out = (wght1* array[1][ind1] + wght2* array[1][ind2]) / 2 
-            Im_n_out = (wght1* array[2][ind1] + wght2* array[2][ind2]) / 2
-            Ref_Indx = Re_n_out + j * Im_n_out
-        return Ref_Indx
-    
-    
-        
 class SpAsil(Grainspecies): 
     
         
         def getSilica_props(en):
             
+            en = en.to(u.micron, equivalencies=u.spectral())
             if en.unit != _u.eV or en.unit != en.J :
                 raise('The unit of Energy should be in eV or Joule')
             elif en.unit == en.J:
