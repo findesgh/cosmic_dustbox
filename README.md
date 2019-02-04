d057fa67965cc24a924358815d687070d7af4c9e
Martin Glatzle <mglatzle@mpa-garching.mpg.de>
2019-02-05 00:17:22 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# cosmic_dustbox
-Toolbox for cosmic dust.
+# A toolbox for cosmic dust.
+In 1930, after confirming that distant stars in the Milky Way are dimmed more
+strongly than one would expect from geometry, Trumpler conjectured the
+existence of "dust particles of various sizes" in the interstellar space of our
+Galaxy. Countless observations have since then confirmed this and the
+"interstellar extinction" remains one of the observational pillars of the study
+of cosmic dust. It was later discovered that there is dust also in
+interplanetary space and that it is important for planet formation, there is
+dust in other galaxies, even far away galaxies, and maybe there also is dust in
+intergalactic space. It was confirmed that dust grains do indeed come in
+different sizes and, moreover, in different shapes and different chemical
+compositions. In short, while many things have been understood, it also became
+apparent that cosmic dust is very complicated. The goal of this software
+package is to make work with models of cosmic dust on the computer easier while
+still providing control over all the involved intricacies. An ambitious goal
+for sure, but we'll try our best.
+
+To introduce the objects central to how we describe cosmic dust, assume we are
+interested in the dust in a certain environment. Assume then that we cut some
+volume out of said environment. The volume should, of course, be representative
+the environment. So if we are interested in the dust in the Solar System as a
+whole, a cubic meter of space somewhere between Earth and Mars will be too
+small and a box the size of the Milky Way will be too large. Suppose we
+collected all of the dust grains contained in this volume. We would like to be
+able to describe this collection with a reasonable degree of accuracy without
+having to store information on every single grain. If we investigated the
+properties of single grains, at first the diversity would probably be
+overwhelming since hardly any two grains would be exactly alike. But after
+looking at enough grains, we would start discovering similarities. Grains that
+have similar structure, grains that look like larger or smaller versions of
+other grains, grains that have similar chemical composition and so on. So if we
+are fine with some level of abstraction and generalization, our grain
+collection could be represented like this:
+
+![](figs/grain-pop.png)
+
+with color-coded chemical composition. How do we go about categorizing these
+grains so that we can think in these categories without worrying about
+individual grains? Two obvious properties to group by are the chemical
+composition and the shape:
+
+![](figs/grain-species.png)
+
+The "blueprint grain", which we can rescale to obtain any of the grains in one
+of the groups, is referred to as a grain species. So one of our groups is
+simply a grain species combined with a size distribution and we call it a grain
+population. If we think back to the abstract representation of our collection
+of grains, it is not very different from one of the groups. It simply has more
+than one grain species, each with its associated size distribution. So to keep
+it simple, we will also call this a grain population.
+
+So, to recap, what we need to describe a collection of grains are:
+
+- grain species
+- size distributions
+- grain populations
+
+This approach can be refined as needed by simply splitting grain species. In
+the extreme, we would have one species per grain and each species would have a
+delta-peak size distribution and we would be back to the overwhelming diversity
+we started out from.
+
+# Literature
+"Physics of the Interstellar and Intergalactic Medium" by B. Draine provides
+a good introduction (and more than that) to cosmic dust.
--- cosmic_dustbox/sdist.py
+++ cosmic_dustbox/sdist.py
@@ -17,22 +17,22 @@ class SizeDist(object):
     Parameters
     ----------
     sizeMin : scalar Quantity [L]
-            Low end size cutoff of the distribution.
+        Low end size cutoff of the distribution.
     sizeMax : scalar Quantity [L]
-            High end size cutoff of the distribution.
+        High end size cutoff of the distribution.
     func : callable
-            Must take a single Quantity with array value of sizes and return
-            1/n_H * dn_gr/da [1/L]. It is called in the limits set by `sizeMin`
-            and `sizeMax` when an instance of this class is called.
+        Must take a single Quantity with array value of sizes and return
+        1/n_H * dn_gr/da [1/L]. It is called in the limits set by `sizeMin`
+        and `sizeMax` when an instance of this class is called.
 
     Attributes
     ----------
     sizeMin : scalar Quantity [L]
-            Directly taken from parameters.
+        Directly taken from parameters.
     sizeMax : scalar Quantity [L]
-            Directly taken from parameters.
+        Directly taken from parameters.
     func : callable
-            Directly taken from parameters.
+        Directly taken from parameters.
 
     References
     ----------
@@ -41,12 +41,11 @@ class SizeDist(object):
     Examples
     --------
     >>> import astropy.units as u
-
     >>> def f(s): \
             return 1.0/s.unit
     >>> a = SizeDist(3.5*u.angstrom, 1*u.micron, f)
     >>> a(_np.logspace(-11, -5, 10)*u.m)
-    <Quantity [ 0., 0., 0., 1., 1., 1., 1., 1., 0., 0.] 1 / m>
+    <Quantity [0., 0., 0., 1., 1., 1., 1., 1., 0., 0.] 1 / m>
     """
     def __init__(self, sizeMin, sizeMax, func):
         """
@@ -67,7 +66,7 @@ class SizeDist(object):
         Parameters
         ----------
         sizes : array-valued Quantity [L]
-                Grain sizes at which to evaluate func.
+            Grain sizes at which to evaluate func.
 
         Returns
         -------
@@ -77,6 +76,96 @@ class SizeDist(object):
         ind = _np.where((sizes >= self.sizeMin) & (sizes <= self.sizeMax))
         r[ind] = self.func(sizes[ind])
         return r
+
+    def __add__(self, other):
+        """
+        Commutative addition of size distributions.
+
+        Overloading of ``+`` operator. Can add an instance of ``SizeDist`` (or
+        subclass thereof), any callable or a scalar to ``self.func``. No check
+        on units is performed. Returns an instance of ``self.__class__``, the
+        ``func`` attribute of which is defined to return the corresponding sum.
+
+        If ``other`` is an instance of ``SizeDist`` (or subclass), take maximum
+        of the two ``sizeMin`` attributes and minimum of the two ``sizeMax``
+        attributes.
+
+        Parameters
+        ----------
+        other : SizeDist, callable or scalar-valued Quantity [1/L]
+            Is added to ``self.func``.
+
+        Returns
+        -------
+         : ``self.__class__``
+            Instance of own class with corresponding ``func`` attribute.
+
+        Examples
+        --------
+        >>> import astropy.units as u
+
+        >>> def f(s): \
+                return 1.0/s.unit
+        >>> a = SizeDist(1*u.angstrom, 1*u.micron, f)
+        >>> b = SizeDist(10*u.angstrom, 10*u.micron, f)
+        >>> c = a + b
+        >>> c(_np.logspace(-11, -4, 10)*u.m)
+        <Quantity [0., 0., 0., 2., 2., 2., 2., 0., 0., 0.] 1 / m>
+        """
+        if issubclass(other.__class__, SizeDist):
+            # find new size limits
+            sizeMin = max(self.sizeMin, other.sizeMin)
+            sizeMax = min(self.sizeMax, other.sizeMax)
+
+            # new differential number density is sum
+            def func(sizes):
+                return self.func(sizes) + other.func(sizes)
+
+            return self.__class__(sizeMin, sizeMax, func)
+        elif callable(other):
+            def func(sizes):
+                return self.func(sizes) + other(sizes)
+            return self.__class__(self.sizeMin, self.sizeMax, func)
+        else:
+            def func(sizes):
+                return other + self.func(sizes)
+            return self.__class__(self.sizeMin, self.sizeMax, func)
+
+    # make addition commutative
+    __radd__ = __add__
+
+    def __mul__(self, other):
+        if callable(other):
+            def func(sizes):
+                return self.function(sizes) * other(sizes)
+            return self.__class__(self.sizeMin, self.sizeMax, func)
+        else:
+            def func(sizes):
+                return other * self.function(sizes)
+            return self.__class__(self.sizeMin, self.sizeMax, func)
+
+    # make multiplication commutative
+    __rmul__ = __mul__
+
+
+class PowerLaw(SizeDist):
+    """
+    Parameters
+    ----------
+    sizeMin : scalar Quantity [L]
+        Low end size cutoff of the distribution.
+    sizeMax : scalar Quantity [L]
+        High end size cutoff of the distribution.
+    power : float
+        Log-slope of the size distribution.
+    C : scalar Quantity [L**(-1-power)]
+        Normalization of the size distribution.
+    """
+    def __init__(self, sizeMin, sizeMax, power, C):
+        def f(a):
+            return C*a**power
+        super(self).__init__(sizeMin, sizeMax, f)
+        return
 
 
 ###############################################################################
