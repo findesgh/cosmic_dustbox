999eb51425ff01700f0825bd0a5c69d3ecd74c59
Martin Glatzle <mglatzle@mpa-garching.mpg.de>
2019-02-23 13:41:57 +0100

    Externalize metadata.

---
+++ metadata.py
@@ -0,0 +1,9 @@
+with open('README.md') as fh:
+    long_desc = fh.read()
+name = 'cosmic_dustbox'
+version = '0.1.dev0'
+authors = 'Martin Glatzle, Ayman Noureldin'
+author_email = 'findessp@yandex.ru'
+description = 'Toolbox for cosmic dust.'
+url = 'https://github.com/findesgh/'+name
+license = 'GPL-3.0'
