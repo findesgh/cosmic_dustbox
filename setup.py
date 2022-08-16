ee1dc7a2659511c8355d0336da0152de53b4f2d4
findesgh <findessp@yandex.ru>
2022-08-16 22:11:28 +0200

    Switch to setup.cfg.

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
 from setuptools import setup
-import metadata as md
 
-setup(
-    name=md.name,
-    version=md.version,
-    author=md.authors,
-    author_email=md.author_email,
-    description=md.description,
-    long_description=md.long_desc,
-    long_description_content_type='text/markdown',
-    url=md.url,
-    license=md.license,
-    packages=[md.name],
-    install_requires=[
-        'numpy',
-        'astropy',
-        'scipy'
-    ],
-    tests_require=[
-        'pytest'
-    ],
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup()
